0a36c84665ba2cd58609cf5beb33f702f3b75adb
colindcli <978478090@qq.com>
2019-09-21 01:04:49 +0800

    -

--- DwrUtility.Test/StartStopwatchTest.cs
+++ DwrUtility.Test/StartStopwatchTest.cs
@@ -27,7 +27,7 @@ namespace DwrUtility.Test
             var t3 = sw.GetTime();
 
             var b1 = t1 >= 100 && t1 < 110;
-            var b2 = t2 >= 200 && t2 < 205;
+            var b2 = t2 >= 199 && t2 < 205;
             var b3 = t3 >= 1000 && t3 < 1005;
 
             Assert.IsTrue(b1 && b2 && b3);
