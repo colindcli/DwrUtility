8b60595d0232b9f39b7fcefc4d519474d623b4e4
COLIN <colindcli@users.noreply.github.com>
2019-09-24 11:51:42 +0800

    -

---
+++ DwrUtility.Test/ConfigHelperTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DwrUtility.Test
+{
+    [TestClass]
+    public class ConfigHelperTest
+    {
+        private static readonly string Root =
+            Path.GetFullPath($"{AppDomain.CurrentDomain.BaseDirectory}/../../DataFiles/config/");
+
+        private static readonly string Temp = $"{AppDomain.CurrentDomain.BaseDirectory}/Temp/";
+
+        public ConfigHelperTest()
+        {
+            if (!Directory.Exists(Temp))
+            {
+                Directory.CreateDirectory(Temp);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var scource = $"{Root}data_1.config";
+            var key = "test";
+            var value = "ok";
+            var value2 = "update";
+
+            var path = $"{Temp}{Guid.NewGuid()}.config";
+            File.Copy(scource, path, true);
+
+            var b1 = string.IsNullOrWhiteSpace(ConfigHelper.GetAppSetting(path, key));
+
+            var b2 = ConfigHelper.UpsetAppSetting(path, key, value);
+            var b3 = ConfigHelper.GetAppSetting(path, key) == value;
+
+            var b4 = ConfigHelper.UpdateAppSetting(path, key, value2);
+            var b5 = ConfigHelper.GetAppSetting(path, key) == value2;
+
+            ConfigHelper.RemoveAppSetting(path, key);
+            var b6 = string.IsNullOrWhiteSpace(ConfigHelper.GetAppSetting(path, key));
+
+            Assert.IsTrue(b1 && b2 && b3 && b4 && b5 && b6);
+
+            File.Delete(path);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var scource = $"{Root}data_1.config";
+            var name = "test";
+            var value = "ok";
+            var value2 = "update";
+
+            var path = $"{Temp}{Guid.NewGuid()}.config";
+            File.Copy(scource, path, true);
+
+            var b1 = string.IsNullOrWhiteSpace(ConfigHelper.GetConnectionString(path, name));
+
+            var b2 = ConfigHelper.UpsetConnectionString(path, name, value);
+            var b3 = ConfigHelper.GetConnectionString(path, name) == value;
+
+            var b4 = ConfigHelper.UpdateConnectionString(path, name, value2);
+            var b5 = ConfigHelper.GetConnectionString(path, name) == value2;
+
+            ConfigHelper.RemoveConnectionString(path, name);
+            var b6 = string.IsNullOrWhiteSpace(ConfigHelper.GetConnectionString(path, name));
+
+            Assert.IsTrue(b1 && b2 && b3 && b4 && b5 && b6);
+
+            File.Delete(path);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var scource = $"{Root}data_2.config";
+            var key = "test";
+            var value = "ok";
+
+            var path = $"{Temp}{Guid.NewGuid()}.config";
+            File.Copy(scource, path, true);
+
+            var b1 = ConfigHelper.UpdateAppSetting(path, key, value);
+            Assert.IsTrue(!b1);
+
+            var b2 = ConfigHelper.UpsetAppSetting(path, key, value);
+            Assert.IsTrue(b2);
+
+            //验证
+            var text = File.ReadAllText(path);
+            Assert.IsTrue(text.Contains("版本") && text.Contains(key) && text.Contains(value));
+
+            File.Delete(path);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var scource = $"{Root}data_3.config";
+            var key = "conn";
+            var value = "ok";
+
+            var path = $"{Temp}{Guid.NewGuid()}.config";
+            File.Copy(scource, path, true);
+
+            var b1 = ConfigHelper.UpdateConnectionString(path, key, value);
+            Assert.IsTrue(!b1);
+
+            var b2 = ConfigHelper.UpsetConnectionString(path, key, value);
+            Assert.IsTrue(b2);
+
+            //验证
+            var text = File.ReadAllText(path);
+            Assert.IsTrue(text.Contains("版本") && text.Contains(key) && text.Contains(value));
+
+            File.Delete(path);
+        }
+    }
+}
---
+++ DwrUtility/ConfigHelper.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Xml;
+
+namespace DwrUtility
+{
+    /// <summary>
+    /// 获取或修改配置文件
+    /// </summary>
+    public class ConfigHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private Configuration Config { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path">web.config或app.config文件路径</param>
+        public ConfigHelper(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path) || !File.Exists(path))
+            {
+                throw new Exception("path文件不存在");
+            }
+
+            var map = new ExeConfigurationFileMap
+            {
+                ExeConfigFilename = path
+            };
+            Config = ConfigurationManager.OpenMappedExeConfiguration(map, ConfigurationUserLevel.None);
+        }
+
+        /// <summary>
+        /// 获取ConnectionString值
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public string GetConnectionString(string name)
+        {
+            return Config.ConnectionStrings.ConnectionStrings[name]?.ConnectionString;
+        }
+
+        /// <summary>
+        /// 获取AppSetting值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string GetAppSetting(string key)
+        {
+            return Config.AppSettings.Settings[key]?.Value;
+        }
+
+        /// <summary>
+        /// 获取ConnectionString值
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static string GetConnectionString(string path, string name)
+        {
+            return new ConfigHelper(path).GetConnectionString(name);
+        }
+
+        /// <summary>
+        /// 获取AppSetting值
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string GetAppSetting(string path, string key)
+        {
+            return new ConfigHelper(path).GetAppSetting(key);
+        }
+
+        #region 公共方法
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="tagName"></param>
+        /// <param name="keyAtrrName"></param>
+        /// <param name="keyAttrValue"></param>
+        /// <param name="valueAttrName"></param>
+        /// <param name="valueAttrValue"></param>
+        private static bool SetValue(string path, string tagName, string keyAtrrName, string keyAttrValue, string valueAttrName, string valueAttrValue)
+        {
+            var xml = new XmlDocument();
+            xml.Load(path);
+            var nodeList = xml.GetElementsByTagName(tagName);
+
+            if (nodeList.Count == 0)
+            {
+                //在configuration下创建一个节点
+                nodeList = xml.GetElementsByTagName("configuration");
+                if (nodeList.Count == 0)
+                {
+                    return false;
+                }
+
+                var nd = nodeList[0];
+                var e = xml.CreateElement(tagName);
+                nd.AppendChild(e);
+
+                //重新获取节点
+                nodeList = xml.GetElementsByTagName(tagName);
+            }
+
+            var node = nodeList[0];
+            var flag = false;
+            foreach (XmlNode item in node)
+            {
+                if (item.NodeType == XmlNodeType.Comment)
+                {
+                    continue;
+                }
+
+                if (item.Attributes == null)
+                {
+                    continue;
+                }
+
+                var attr = item.Attributes[keyAtrrName];
+                if (!string.Equals(attr.Value, keyAttrValue, StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+
+                item.Attributes[valueAttrName].Value = valueAttrValue;
+
+                //保存
+                xml.Save(path);
+                flag = true;
+                break;
+            }
+
+            return flag;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="tagName"></param>
+        /// <param name="keyAtrrName"></param>
+        /// <param name="keyAttrValue"></param>
+        /// <param name="valueAttrName"></param>
+        /// <param name="valueAttrValue"></param>
+        private static bool AddValue(string path, string tagName, string keyAtrrName, string keyAttrValue, string valueAttrName, string valueAttrValue)
+        {
+            var xml = new XmlDocument();
+            xml.Load(path);
+            var nodeList = xml.GetElementsByTagName(tagName);
+
+            if (nodeList.Count == 0)
+            {
+                //在configuration下创建一个节点
+                nodeList = xml.GetElementsByTagName("configuration");
+                if (nodeList.Count == 0)
+                {
+                    return false;
+                }
+
+                var nd = nodeList[0];
+                var e = xml.CreateElement(tagName);
+                nd.AppendChild(e);
+
+                //重新获取节点
+                nodeList = xml.GetElementsByTagName(tagName);
+            }
+
+            var node = nodeList[0];
+            var exists = false;
+            foreach (XmlNode item in node)
+            {
+                if (item.NodeType == XmlNodeType.Comment)
+                {
+                    continue;
+                }
+
+                var attr = item.Attributes?[keyAtrrName];
+                if (attr == null)
+                {
+                    continue;
+                }
+
+                if (!string.Equals(attr.Value, keyAttrValue, StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+
+                item.Attributes[valueAttrName].Value = valueAttrValue;
+                exists = true;
+                break;
+            }
+
+            //已存在保存
+            if (exists)
+            {
+                xml.Save(path);
+                return true;
+            }
+
+            //不存在添加
+            var el = xml.CreateElement("add");
+            el.SetAttribute(keyAtrrName, keyAttrValue);
+            el.SetAttribute(valueAttrName, valueAttrValue);
+            node.AppendChild(el);
+            xml.Save(path);
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="tagName"></param>
+        /// <param name="keyAtrrName"></param>
+        /// <param name="keyAttrValue"></param>
+        private static void RemoveValue(string path, string tagName, string keyAtrrName, string keyAttrValue)
+        {
+            var xml = new XmlDocument();
+            xml.Load(path);
+            var nodeList = xml.GetElementsByTagName(tagName);
+
+            if (nodeList.Count == 0)
+            {
+                return;
+            }
+
+            var node = nodeList[0];
+            foreach (XmlNode item in node)
+            {
+                if (item.NodeType == XmlNodeType.Comment)
+                {
+                    continue;
+                }
+
+                var attr = item.Attributes?[keyAtrrName];
+                if (attr == null)
+                {
+                    continue;
+                }
+
+                if (!string.Equals(attr.Value, keyAttrValue, StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+
+                node.RemoveChild(item);
+
+                //保存
+                xml.Save(path);
+                break;
+            }
+        }
+
+        #endregion
+
+        #region connectionStrings
+
+        /// <summary>
+        /// 设置ConnectionString值
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="name"></param>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        public static bool UpdateConnectionString(string path, string name, string connectionString)
+        {
+            return SetValue(path, "connectionStrings", "name", name, "connectionString", connectionString);
+        }
+
+        /// <summary>
+        /// 添加ConnectionString或更新ConnectionString值
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="name"></param>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        public static bool UpsetConnectionString(string path, string name, string connectionString)
+        {
+            return AddValue(path, "connectionStrings", "name", name, "connectionString", connectionString);
+        }
+
+        /// <summary>
+        /// 删除ConnectionString
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static void RemoveConnectionString(string path, string name)
+        {
+            RemoveValue(path, "connectionStrings", "name", name);
+        }
+
+
+        #endregion
+
+        #region appSettings
+
+        /// <summary>
+        /// 设置AppSetting值
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static bool UpdateAppSetting(string path, string key, string value)
+        {
+            return SetValue(path, "appSettings", "key", key, "value", value);
+        }
+
+        /// <summary>
+        /// 添加AppSetting或更新AppSetting值
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static bool UpsetAppSetting(string path, string key, string value)
+        {
+            return AddValue(path, "appSettings", "key", key, "value", value);
+        }
+
+        /// <summary>
+        /// 删除AppSetting
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="key"></param>
+        public static void RemoveAppSetting(string path, string key)
+        {
+            RemoveValue(path, "appSettings", "key", key);
+        }
+
+        #endregion
+    }
+}
